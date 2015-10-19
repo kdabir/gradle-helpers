b3e8d56d56e34fdcc7909770e30c619465567d66
Kunal Dabir <kunal.dabir@gmail.com>
2015-10-19 13:35:54 +0530

    add wrapper to project and javadocs

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # Gradle Helpers
 
+[![Build Status](https://travis-ci.org/kdabir/gradle-helpers.svg?branch=master)](https://travis-ci.org/kdabir/gradle-helpers)
+
 Exposes utility methods under `helpers` namespace. Does not touch any other part of build.
+
+
  
 
--- src/io/github/kdabir/gradlehelpers/HelpersExtension.groovy
+++ src/io/github/kdabir/gradlehelpers/HelpersExtension.groovy
@@ -10,15 +10,33 @@ class HelpersExtension {
         this.project = project
     }
 
+    /**
+     * lists available methods on an object
+     *
+     * @param obj
+     * @return list of method names
+     */
     def methodsOf(obj) {
         obj.metaClass.methods*.name.sort().unique()
     }
 
-    def loadProperties(String filename) {
+    /**
+     * loads properties from relative to project root
+     *
+     * @param filename - String
+     * @return properties object
+     */
+    Properties loadProperties(String filename) {
         loadProperties(project.file(filename))
     }
 
-    def loadProperties(File file) {
+    /**
+     * loads properties from file
+     *
+     * @param file - File Object
+     * @return properties object
+     */
+    Properties loadProperties(File file) {
         Properties properties = new Properties()
         file.withInputStream { properties.load(it) }
         return properties
